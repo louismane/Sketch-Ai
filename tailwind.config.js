d066e3a1b7535de793419e9a812e14d34b0d8e87
louismane <nwachukwusolo328@gmail.com>
2025-12-26 15:05:15 -0600

    fix: use CommonJS syntax for tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,5 @@
 /** @type {import('tailwindcss').Config} */
-export default {
+module.exports = {
   darkMode: 'class',
   content: [
     './index.html',
@@ -77,4 +77,4 @@ export default {
     },
   },
   plugins: [],
-}
+};
